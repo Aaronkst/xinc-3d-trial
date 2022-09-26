49d4013140d27709f374157a57b24f0822e354ff
Aaronkst <aaron.kst99@gmail.com>
2022-09-26 16:57:09 +0630

    fixed movements

--- src/modules/charcterControls.ts
+++ src/modules/charcterControls.ts
@@ -1,53 +1,31 @@
 import * as THREE from "three";
+import * as TWEEN from "@tweenjs/tween.js";
 
-interface Movement {
-  xInterval?: ReturnType<typeof setInterval>;
-  zInterval?: ReturnType<typeof setInterval>;
-  callbackInterval?: ReturnType<typeof setInterval>;
-}
-
-/**
- * Convert radians to degrees
- * @param r radians
- * @returns degress
- */
-const r2d = (r: number): number => {
-  return r * (180 / Math.PI);
+const animate = (time: number) => {
+  requestAnimationFrame(animate);
+  TWEEN.update(time);
 };
-
-/**
- * Convert degrees to radians
- * @param d degrees
- * @returns radians
- */
-const d2r = (d: number): number => {
-  return d * (Math.PI / 180);
-};
-
+requestAnimationFrame(animate);
 export default class CharacterControls {
   model: THREE.Group;
   mixer: THREE.AnimationMixer;
   animationsMap: Map<string, THREE.AnimationAction> = new Map(); //Walk, Run, Idle
   currentAnimation: THREE.AnimationAction | undefined;
-  movement: Movement;
-  moveunit = 0.03;
-  velocity = 1;
+  movement?: TWEEN.Tween<THREE.Vector3>;
+  velocity: number;
 
   constructor(
     model: THREE.Group,
     mixer: THREE.AnimationMixer,
     animationsMap: Map<string, THREE.AnimationAction>,
-    currentAction: string
+    currentAction: string,
+    speed: number = 3
   ) {
     this.model = model;
     this.mixer = mixer;
     this.animationsMap = animationsMap;
-    this.animationsMap.forEach((value, key) => {
-      if (key == currentAction) {
-        value.play();
-      }
-    });
-    this.movement = {};
+    this.animationsMap.get(currentAction)?.play();
+    this.velocity = speed;
   }
 
   private setAnimation(animation: string) {
@@ -59,9 +37,7 @@ export default class CharacterControls {
   }
 
   public reset() {
-    clearInterval(this.movement.xInterval);
-    clearInterval(this.movement.zInterval);
-    clearInterval(this.movement.callbackInterval);
+    this.movement?.stop();
 
     this.setAnimation("Idle");
 
@@ -71,70 +47,20 @@ export default class CharacterControls {
   }
 
   public move(point: THREE.Vector3) {
-    clearInterval(this.movement.xInterval);
-    clearInterval(this.movement.zInterval);
-    clearInterval(this.movement.callbackInterval);
-
+    this.movement?.stop();
     this.model.lookAt(point);
     this.model.rotateY(Math.PI);
     this.setAnimation("Walk");
 
-    const startx = this.model.position.x;
-    const startz = this.model.position.z;
+    const distance = this.model.position.distanceTo(point);
 
-    const distx = point.x > startx ? point.x - startx : startx - point.x;
-    const distz = point.z > startz ? point.z - startz : startz - point.z;
+    this.movement = new TWEEN.Tween(this.model.position).to(
+      point,
+      (distance / this.velocity) * 1000
+    );
 
-    const timex = distx / this.velocity;
-    const timez = distz / this.velocity;
+    this.movement.start();
 
-    console.log("timex", timex, "timez", timez);
-
-    const isArrived = { x: false, z: false };
-
-    this.movement.xInterval = setInterval(() => {
-      if (startx > point.x) {
-        if (this.model.position.x <= point.x) {
-          clearInterval(this.movement.xInterval);
-        }
-        this.model.position.x = this.model.position.x - this.moveunit;
-      } else {
-        if (this.model.position.x >= point.x) {
-          clearInterval(this.movement.xInterval);
-        }
-        this.model.position.x = this.model.position.x + this.moveunit;
-      }
-    }, 100 / timex);
-
-    this.movement.zInterval = setInterval(() => {
-      if (startz > point.z) {
-        if (this.model.position.z <= point.z) {
-          clearInterval(this.movement.zInterval);
-        }
-        this.model.position.z = this.model.position.z - this.moveunit;
-      } else {
-        if (this.model.position.z >= point.z) {
-          clearInterval(this.movement.zInterval);
-        }
-        this.model.position.z = this.model.position.z + this.moveunit;
-      }
-    }, 100 / timez);
-
-    this.movement.callbackInterval = setInterval(() => {
-      if (startx > point.x) {
-        if (this.model.position.x <= point.x) isArrived.x = true;
-      } else {
-        if (this.model.position.x >= point.x) isArrived.x = true;
-      }
-      if (startz > point.z) {
-        if (this.model.position.z <= point.z) isArrived.z = true;
-      } else {
-        if (this.model.position.z >= point.z) isArrived.z = true;
-      }
-      if (!!isArrived.x && !!isArrived.z) {
-        this.setAnimation("Idle");
-        clearInterval(this.movement.callbackInterval);
-      }
-    }, 100);
+    this.movement.onComplete(() => this.setAnimation("Idle"));
   }
 }
